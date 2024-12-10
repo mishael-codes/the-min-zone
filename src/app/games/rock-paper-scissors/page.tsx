18301463e0f45bb6b119b06103da7a304c0c0ee1
mishael-codes <enyimishael22@gmail.com>
2024-12-10 11:28:53 +0100

    Feat: Add levels to rock paper scissors

--- src/app/games/rock-paper-scissors/page.tsx
+++ src/app/games/rock-paper-scissors/page.tsx
@@ -5,7 +5,9 @@ import { faHandRock } from "@fortawesome/free-solid-svg-icons";
 import { faHandScissors } from "@fortawesome/free-solid-svg-icons";
 import { faHandPaper } from "@fortawesome/free-solid-svg-icons";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Button from "@/app/components/button";
+import { motion } from "framer-motion";
 const RockPaperScissor = () => {
   const choices = ["rock", "paper", "scissors"];
   // randomize the computer's choice from the start
@@ -16,6 +18,63 @@ const RockPaperScissor = () => {
   const [computerPoints, setComputerPoints] = useState(0);
   const [color, setColor] = useState("");
   const [feedback, setFeedback] = useState("");
+  const [level, setLevel] = useState(0);
+  const [customLevel, setCustomLevel] = useState(0);
+  const [max, setMax] = useState(0);
+
+  useEffect(() => {
+    if (level === 3) {
+      setMax(3);
+    } else if (level === 5) {
+      setMax(5);
+    } else if (level === customLevel) {
+      setMax(customLevel);
+    }
+  }, [level, customLevel]);
+
+  useEffect(() => {
+    // max level is 3
+    if (max === 3) {
+      if (userPoints + computerPoints === max) {
+        if (userPoints > computerPoints) {
+          alert("You win");
+        } else if (userPoints < computerPoints) {
+          alert("You lose");
+        } else if (userPoints === computerPoints) {
+          alert("It's a draw");
+        }
+      }
+    }
+    // max level is 5
+    if (max === 5) {
+      if (userPoints + computerPoints === max) {
+        if (userPoints > computerPoints) {
+          alert("You win");
+        } else if (userPoints < computerPoints) {
+          alert("You lose");
+        } else if (userPoints === computerPoints) {
+          alert("It's a draw");
+        }
+      }
+    }
+    // max level is custom
+    if (customLevel && max === customLevel) {
+      if (userPoints + computerPoints === customLevel) {
+        if (userPoints > computerPoints) {
+          alert("You win");
+        } else if (userPoints < computerPoints) {
+          alert("You lose");
+        } else if (userPoints === computerPoints) {
+          alert("It's a draw");
+        }
+      }
+    }
+  }, [computerPoints, customLevel, max, userPoints]);
+
+  const handleCustomLevelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setCustomLevel(parseFloat(value));
+  };
 
   const pickYourChampion = (item: string) => {
     // on each click or choice made by user, re-choose the computer's choice
@@ -89,45 +148,77 @@ const RockPaperScissor = () => {
 
   return (
     <main className="flex flex-col items-center justify-between py-20">
-      <h1 className="flex items-start justify-center flex-col w-fit font-semibold text-3xl">
-        <span className="first-letter:text-orange-500">Rock</span>{" "}
-        <span className="first-letter:text-orange-500">Paper</span>{" "}
-        <span className="first-letter:text-orange-500">Scissors</span>
-      </h1>
-      <p className={`font-semibold ${color}`}>{feedback}</p>
-      <div className="flex flex-col md:flex-row items-center justify-evenly mt-32 w-full transition-all">
-        <div
-          onClick={() => pickYourChampion("rock")}
-          className=" text-orange-500"
-        >
-          <FontAwesomeIcon
-            icon={faHandRock}
-            className="p-5 scale-[3] border-2 border-gray-600 border-dashed rounded-full cursor-pointer hover:border-orange-500"
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+        className={`flex items-center justify-center flex-col bg-black text-[#f4f4f4] gap-7 p-12 rounded-lg shadow-lg shadow-orange-400 border-2 border-orange-500 ${
+          level ? `hidden` : `flex`
+        }`}
+      >
+        <Button text="Rounds of 3" click={() => setLevel(3)} standard />
+        <Button text="Rounds of 5" click={() => setLevel(5)} standard />
+        <div className="flex items-center justify-center">
+          <Button
+            text="Set"
+            click={() => {
+              setLevel(customLevel);
+            }}
+            standard
+          />
+          <input
+            type="number"
+            placeholder="Enter number of rounds"
+            className="border-2 border-orange-500 p-2 rounded-md w-fit ml-2 bg-transparent text-orange-500"
+            onChange={handleCustomLevelChange}
           />
         </div>
-        <div
-          onClick={() => pickYourChampion("paper")}
-          className=" text-orange-500 my-36 md:my-0"
-        >
-          <FontAwesomeIcon
-            icon={faHandPaper}
-            className="p-5 scale-[3] border-2 border-gray-600 border-dashed rounded-full cursor-pointer hover:border-orange-500"
-          />
-        </div>
-        <div
-          onClick={() => pickYourChampion("scissors")}
-          className=" text-orange-500"
-        >
-          <FontAwesomeIcon
-            icon={faHandScissors}
-            className="p-5 scale-[3] border-2 border-gray-600 border-dashed rounded-full cursor-pointer hover:border-orange-500"
-          />
-        </div>
-      </div>
-      <div className="mt-32">
-        <p>You: {userPoints}</p>
-        <p>Computer: {computerPoints}</p>
-      </div>
+      </motion.div>
+      {level ? (
+        <>
+          <h1 className="flex items-start justify-center flex-col w-fit font-semibold text-3xl">
+            <span className="first-letter:text-orange-500">Rock</span>{" "}
+            <span className="first-letter:text-orange-500">Paper</span>{" "}
+            <span className="first-letter:text-orange-500">Scissors</span>
+          </h1>
+          <p className={`font-semibold ${color}`}>{feedback}</p>
+          <div className="flex flex-col md:flex-row items-center justify-evenly mt-32 w-full transition-all">
+            <div
+              onClick={() => pickYourChampion("rock")}
+              className=" text-orange-500"
+            >
+              <FontAwesomeIcon
+                icon={faHandRock}
+                className="p-5 scale-[3] border-2 border-gray-600 border-dashed rounded-full cursor-pointer hover:border-orange-500"
+              />
+            </div>
+            <div
+              onClick={() => pickYourChampion("paper")}
+              className=" text-orange-500 my-36 md:my-0"
+            >
+              <FontAwesomeIcon
+                icon={faHandPaper}
+                className="p-5 scale-[3] border-2 border-gray-600 border-dashed rounded-full cursor-pointer hover:border-orange-500"
+              />
+            </div>
+            <div
+              onClick={() => pickYourChampion("scissors")}
+              className=" text-orange-500"
+            >
+              <FontAwesomeIcon
+                icon={faHandScissors}
+                className="p-5 scale-[3] border-2 border-gray-600 border-dashed rounded-full cursor-pointer hover:border-orange-500"
+              />
+            </div>
+          </div>
+          <div className="mt-32">
+            <p>You: {userPoints}</p>
+            <p>Computer: {computerPoints}</p>
+          </div>
+        </>
+      ) : (
+        ""
+      )}
     </main>
   );
 };
