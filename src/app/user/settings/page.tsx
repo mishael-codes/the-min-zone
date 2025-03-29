61aa0e68f77c98bbedb2ad44779e06f4e7cc0e77
mishael-codes <enyimishael22@gmail.com>
2025-03-30 00:55:38 +0100

    Feat: add features that edit and update the username, update the settings page

--- src/app/user/settings/page.tsx
+++ src/app/user/settings/page.tsx
@@ -1,15 +1,24 @@
+"use client";
 // firebase imports
 import auth from "../../../firebase/fire-auth";
-import { signOut } from "firebase/auth";
+import { signOut, updateProfile } from "firebase/auth";
 
 // next imports
 import { useRouter } from "next/navigation";
 
+import { useAuth } from "@/context/AuthContext";
+import Button from "@/app/components/button";
+import { useState } from "react";
+import { motion } from "framer-motion";
+
 const Settings = () => {
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
   const router = useRouter();
+  const { user } = useAuth();
 
   // sign out user
-  const signOutUser = () => {
+  const handleSignOut = () => {
     signOut(auth)
       .then(() => {
         router.push("/auth/sign-in");
@@ -19,8 +28,80 @@ const Settings = () => {
       });
   };
 
+  // set username
+  const handleSetUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setUsername(value);
+    console.log(username);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      // update username in firebase
+
+      if (auth.currentUser) {
+        if (username) {
+          await updateProfile(auth.currentUser, {
+            displayName: username,
+          });
+          setMessage("Username updated successfully");
+          setTimeout(() => {
+            setMessage("");
+          }, 3000);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <><button onClick={signOutUser}>Sign out</button></>
-  )
-}
-export default Settings
+    <div className="ml-10">
+      {user ? (
+        <>
+          <h1 className="text-5xl font-bold mt-20">Settings</h1>
+          <form
+            className="flex items-start justify-center flex-col gap-5 my-10 w-full md:w-3/4"
+            onSubmit={handleSubmit}
+          >
+            <label className="flex flex-col font-light w-full md:w-1/3 gap-2">
+              Username
+              <input
+                type="text"
+                onChange={handleSetUsername}
+                placeholder={`${user.displayName || "Set username"}`}
+                className="w-full bg-transparent text-orange-500 p-3 rounded-xl border border-orange-500 focus:border-orange-500 placeholder:text-gray-500"
+              />
+              {message ? (
+                <motion.p
+                  {...{ initial: { opacity: 0 }, animate: { opacity: 1 } }}
+                  transition={{ duration: 0.5 }}
+                  className="text-xs"
+                >
+                  {message}
+                </motion.p>
+              ) : (
+                ""
+              )}
+            </label>
+            <label className="flex flex-col font-light w-full md:w-1/3 gap-2">
+              Email
+              <input
+                type="email"
+                placeholder={`${user.email}`}
+                className="w-full bg-transparent text-orange-500 p-3 rounded-xl border border-orange-500 focus:border-orange-500 placeholder:text-gray-500"
+              />
+            </label>
+            <Button text="Save" standard />
+          </form>
+          <Button text="Sign Out" click={() => handleSignOut} />
+        </>
+      ) : (
+        <p>You are not signed in</p>
+      )}
+    </div>
+  );
+};
+export default Settings;
